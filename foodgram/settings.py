3e4f49018f407124a8af20457d4d27d48e01d8b6
Aleksei Libman <lexlibman@gmail.com>
2021-04-14 23:21:46 +0300

    to_deploy

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -1,10 +1,8 @@
 import os
 
-from django.core.management.utils import get_random_secret_key
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
