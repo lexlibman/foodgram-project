ce08e946a263b7346db6a9f3ca6ddb8a1a49e23e
Aleksei Libman <lexlibman@gmail.com>
2021-04-14 23:46:05 +0300

    to_deploy

--- recipes/utils.py
+++ recipes/utils.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 
-from django.db import transaction
+from django.db import transaction, IntegrityError
+from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 
 from .models import Ingredient, RecipeIngredient
@@ -14,31 +15,40 @@ def get_ingredients(request):
             ingredients[name] = request.POST[
                 f'valueIngredient_{num}'
             ]
+
     return ingredients
 
 
 def save_recipe(request, form):
-    with transaction.atomic():
-        recipe = form.save(commit=False)
-        recipe.author = request.user
-        recipe.save()
-        objs = []
-        ingredients = get_ingredients(request)
-        for name, quantity in ingredients.items():
-            ingredient = get_object_or_404(Ingredient, title=name)
-            objs.append(
-                RecipeIngredient(
-                    recipe=recipe,
-                    ingredient=ingredient,
-                    quantity=Decimal(quantity.replace(',', '.'))
+    try:
+        with transaction.atomic():
+            recipe = form.save(commit=False)
+            recipe.author = request.user
+            recipe.save()
+
+            objs = []
+            ingredients = get_ingredients(request)
+            for name, quantity in ingredients.items():
+                ingredient = get_object_or_404(Ingredient, title=name)
+                objs.append(
+                    RecipeIngredient(
+                        recipe=recipe,
+                        ingredient=ingredient,
+                        quantity=Decimal(quantity.replace(',', '.'))
+                    )
                 )
-            )
-        RecipeIngredient.objects.bulk_create(objs)
-        form.save_m2m()
-        return recipe
+            RecipeIngredient.objects.bulk_create(objs)
+
+            form.save_m2m()
+            return recipe
+    except IntegrityError:
+        raise HttpResponseBadRequest
 
 
 def edit_recipe(request, form, instance):
-    with transaction.atomic():
-        RecipeIngredient.objects.filter(recipe=instance).delete()
-        return save_recipe(request, form)
+    try:
+        with transaction.atomic():
+            RecipeIngredient.objects.filter(recipe=instance).delete()
+            return save_recipe(request, form)
+    except IntegrityError:
+        raise HttpResponseBadRequest
