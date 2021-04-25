d1f00b7299c02dbbabd29d9e4416e2c91a5b61ce
Aleksei Libman <lexlibman@gmail.com>
2021-04-25 18:42:17 +0300

    it is working

--- recipes/templatetags/user_filters.py
+++ recipes/templatetags/user_filters.py
@@ -1,8 +1,6 @@
 from django import template
 from django.contrib.auth import get_user_model
 
-from recipes.models import Purchase, Favorite, Recipe
-
 register = template.Library()
 User = get_user_model()
 
@@ -10,21 +8,6 @@ User = get_user_model()
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={'class': css})
-
-
-@register.filter
-def is_subscribed_to(user, author):
-    return Recipe.objects.filter(author=author).get_additional_attributes(user)
-
-
-@register.filter
-def is_favored_by(recipe, user):
-    return Favorite.objects.filter(recipe=recipe, user=user)
-
-
-@register.filter
-def is_in_shop_list_of(recipe, user):
-    return Purchase.objects.filter(recipe=recipe, user=user)
 
 
 @register.filter
