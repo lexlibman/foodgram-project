470406372086212ec8ed157c91682346c9429c2e
Aleksei Libman <lexlibman@gmail.com>
2021-04-25 21:12:17 +0300

    templates refactoring

--- foodgram/views.py
+++ foodgram/views.py
@@ -7,7 +7,3 @@ def page_not_found(request, exception):
 
 def server_error(request):
     return render(request, 'error/500.html', status=500)
-
-
-def page_bad_request(request, exception):
-    return render(request, 'error/400.html', status=400)
