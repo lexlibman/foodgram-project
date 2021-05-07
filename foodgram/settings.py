a3d0e4dc8a73d0abc316c008277c144a7fb38754
Aleksei Libman <lexlibman@gmail.com>
2021-05-07 10:05:52 +0300

    tags experiment

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
